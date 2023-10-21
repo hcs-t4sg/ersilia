b7eecea68c4d04ea86b8afb2c9d129b98e14a06d
Holden Schermer <holden@dhcp-10-250-73-21.harvard.edu>
2023-10-21 16:31:14 -0400

    creates current_session.txt

--- ersilia/cli/commands/close.py
+++ ersilia/cli/commands/close.py
@@ -1,3 +1,5 @@
+import datetime
+import os
 from . import ersilia_cli
 from .. import echo
 from ... import ErsiliaModel
@@ -17,3 +19,5 @@ def close_cmd():
         mdl = ErsiliaModel(model_id, service_class=service_class)
         mdl.close()
         echo(":no_entry: Model {0} closed".format(mdl.model_id), fg="green")
+
+      
